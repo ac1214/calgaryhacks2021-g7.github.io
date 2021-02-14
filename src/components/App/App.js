da34bd650f6a6f628713391566a1c9bb4a119332
Justin Flores <justin.flores@ucalgary.ca>
2021-02-13 21:41:34 -0700

    refactored routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,58 @@
-import React from "react";
-import { AuthProvider } from "../../context/auth-context";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
-import PrivateRoute from "../../context/PrivateRoute";
-import Admin from "../../layouts/Admin";
-import Login from "../Login/Login";
+import React, { useContext, useEffect } from "react";
+import { AuthContext, AuthProvider } from "../../context/auth-context";
+import { Route, Switch, withRouter } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import { routes } from "../../routes";
+import Modal from "@material-ui/core/Modal";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import app from "../../lib/Firebase";
+import firebase from "firebase";
+import Admin from "../../layouts/Admin";
 
 const hist = createBrowserHistory();
 
 const App = (props) => {
+  const { user } = useContext(AuthContext);
+
+  const uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      // Avoid redirects after sign-in.
+      signInSuccessWithAuthResult: () => {
+        return false;
+      },
+    },
+  };
+
+  useEffect(() => {
+    console.log(props);
+    if (!routes.includes(props.location.pathname)) {
+      props.history.push("/dashboard");
+    }
+  }, [props]);
+
   return (
-    <AuthProvider>
-      <Router history={hist}>
-        <Route path="/login" component={Login} />
-        <PrivateRoute path="/admin" component={Admin} />
-        <Redirect from="/" to="/admin/dashboard" />
-      </Router>
-    </AuthProvider>
+    <div>
+      <Modal
+        open={!user}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div>
+          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={app.auth()} />
+        </div>
+      </Modal>
+      <Switch>
+        <Route
+          path="*"
+          render={(routeProps) => {
+            return <Admin {...routeProps} />;
+          }}
+        />
+      </Switch>
+    </div>
   );
 };
 
-export default App;
+export default withRouter(App);
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
 import BubbleChart from "@material-ui/icons/BubbleChart";
-import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
 import Notifications from "@material-ui/icons/Notifications";
 
 // core components/views for Admin layout
@@ -15,7 +15,15 @@ import Icons from "views/Icons/Icons.js";
 import Calendar from "views/Calendar/Calendar.js";
 import NotificationsPage from "views/Notifications/Notifications.js";
 
-
+export const routes = [
+  "/dashboard",
+  "/user",
+  "/table",
+  "/typography",
+  "/icons",
+  "/Calendar",
+  "/notifications",
+];
 
 const dashboardRoutes = [
   {
@@ -23,49 +31,49 @@ const dashboardRoutes = [
     name: "Dashboard",
     icon: Dashboard,
     component: DashboardPage,
-    layout: "/admin"
+    layout: "",
   },
   {
     path: "/user",
     name: "User Profile",
     icon: Person,
     component: UserProfile,
-    layout: "/admin"
+    layout: "",
   },
   {
     path: "/table",
     name: "Table List",
     icon: "content_paste",
     component: TableList,
-    layout: "/admin"
+    layout: "",
   },
   {
     path: "/typography",
     name: "Typography",
     icon: LibraryBooks,
     component: Typography,
-    layout: "/admin"
+    layout: "",
   },
   {
     path: "/icons",
     name: "Icons",
     icon: BubbleChart,
     component: Icons,
-    layout: "/admin"
+    layout: "",
   },
   {
     path: "/Calendar",
     name: "Calendar",
     icon: CalendarTodayIcon,
     component: Calendar,
-    layout: "/admin"
+    layout: "",
   },
   {
     path: "/notifications",
     name: "Notifications",
     icon: Notifications,
     component: NotificationsPage,
-    layout: "/admin"
+    layout: "",
   },
 ];
 
