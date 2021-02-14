2d0e41aea11545236941a2dbcf3607db74304810
ac1214 <ac1214@users.noreply.github.com>
2021-02-14 03:32:48 -0700

    updates

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 // @material-ui/icons
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
-import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import BubbleChart from "@material-ui/icons/BubbleChart";
 import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
 import Notifications from "@material-ui/icons/Notifications";
 
@@ -9,7 +10,10 @@ import Notifications from "@material-ui/icons/Notifications";
 import DashboardPage from "views/Dashboard/Dashboard.js";
 import UserProfile from "views/UserProfile/UserProfile.js";
 import TableList from "views/TableList/TableList.js";
+import Typography from "views/Typography/Typography.js";
+import Icons from "views/Icons/Icons.js";
 import Calendar from "views/Calendar/Calendar.js";
+import NotificationsPage from "views/Notifications/Notifications.js";
 import Room from "./views/Room/Room";
 
 export const routes = [
@@ -20,37 +24,15 @@ export const routes = [
   "/icons",
   "/Calendar",
   "/notifications",
-  "/Room",
+  "/Room"
 ];
 
 const dashboardRoutes = [
-  // {
-  //   path: "/dashboard",
-  //   name: "Dashboard",
-  //   icon: Dashboard,
-  //   component: DashboardPage,
-  //   layout: "",
-  // },
   {
-    path: "/table",
-    name: "Table List",
-    icon: "content_paste",
-    component: TableList,
-    layout: "",
-  },
-  {
-    path: "/Calendar",
-    name: "Calendar",
-    icon: CalendarTodayIcon,
-    component: Calendar,
-    layout: "",
-  },
-
-  {
-    path: "/Room",
-    name: "Room",
-    icon: MeetingRoomIcon,
-    component: Room,
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: Dashboard,
+    component: DashboardPage,
     layout: "",
   },
   {
@@ -60,6 +42,48 @@ const dashboardRoutes = [
     component: UserProfile,
     layout: "",
   },
+  {
+    path: "/table",
+    name: "Table List",
+    icon: "content_paste",
+    component: TableList,
+    layout: "",
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    icon: LibraryBooks,
+    component: Typography,
+    layout: "",
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: BubbleChart,
+    component: Icons,
+    layout: "",
+  },
+  {
+    path: "/Calendar",
+    name: "Calendar",
+    icon: CalendarTodayIcon,
+    component: Calendar,
+    layout: "",
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: Notifications,
+    component: NotificationsPage,
+    layout: "",
+  },
+  {
+    path: "/Room",
+    name: "Room",
+    icon: Notifications,
+    component: Room,
+    layout: ""
+  }
 ];
 
 export default dashboardRoutes;
