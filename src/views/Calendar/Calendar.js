b762bb27fae6bd85fef6e87b5445775b8915e4c0
James Peralta <jamesperalta35@gmail.com>
2021-02-14 01:38:42 -0700

    Whack modal going

--- src/views/Calendar/Calendar.js
+++ src/views/Calendar/Calendar.js
@@ -1,28 +1,105 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-// import '@fullcalendar/core/main.css';
-// import '@fullcalendar/daygrid/main.css';
-// import '@fullcalendar/timegrid/main.css';
+import {Button, Modal} from "@material-ui/core";
+import "./Calendar.css";
 
+let valid_slots = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+
+function tConvert(time) {
+    if (time < 12) {
+        return time.toString() + "AM"
+    }
+    else if (time === 12) {
+        return time.toString() + "PM"
+    }
+    else {
+        return (time - 12).toString() + "PM"
+    }
+}
 
 export default function Calendar () {
+    const [chosenTime, setChosenTime] = useState({
+        date: null,
+        dateString: ""
+    });
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleModalOpen = () => {
+        setModalOpen(true);
+    }
+
+    const handleModalClose = () => {
+        setModalOpen(false);
+    }
+
+    const handleEventClick = (event) => {
+        const eventDate = event.event.start;
+        let month = monthNames[eventDate.getMonth()];
+        let day = eventDate.getDate().toString();
+        let hour = tConvert(eventDate.getHours());
+        let completeTime = month + ' ' + day + ' @ ' + hour;
+
+        setChosenTime({
+            date: eventDate,
+            dateString: completeTime
+        });
+    }
+
+    // const handleConfirmClick = () => {
+    //     // Submit matchmaking post request
+    //     alert(chosenTime.date);
+    // }
+
     return (
-      <FullCalendar
-        initialView="timeGridWeek"
-        header={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
-        }}
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        handleWindowResize={true}
-        events={[
-          { title: 'event 1', date: '2021-02-13' },
-          { title: 'event 2', date: '2021-02-12' }
-        ]}
-      />
+        <div className="booking">
+            <FullCalendar
+                initialView="timeGridWeek"
+                header={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+                }}
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                selectable={true}
+                handleWindowResize={true}
+                allDaySlot={false}
+                slotDuration="01:00:00"
+                slotMinTime="08:00:00"
+                slotMaxTime="21:00:00"
+                expandRows={true}
+                eventClick={handleEventClick}
+                eventTimeFormat={{
+                    hour: 'numeric',
+                    minute: '2-digit'
+                }}
+                displayEventEnd={false}
+                events={valid_slots.map(elem => {
+                    return {
+                        groupId: 'blueEvents', // recurrent events in this group move together
+                        daysOfWeek: ['0', '1', '2', '3', '4', '5', '6'],
+                        startTime: elem.toString() + ':00:00',
+                        endTime: (elem + 1).toString() + ':00:00'
+                    }
+                })}
+                editable={true}
+            />
+            <Button className="confirm-button" variant="contained" color="primary" onClick={()=>handleModalOpen()}>
+                {"CONFIRM & SCHEDULE:  " + chosenTime.dateString}
+            </Button>
+            <Modal
+                className="modal"
+                open={modalOpen}
+                onClose={handleModalClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                <h1>Hello World</h1>
+            </Modal>
+        </div>
     )
 }
---
+++ src/views/Calendar/SubjectOptions.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function SubjectOptions(){
+    return (
+        <div>
+        </div>
+    )
+}
