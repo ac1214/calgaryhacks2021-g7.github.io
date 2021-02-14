d55e6050921dea15cfc3a4a5111f9e27fc772216
ac1214 <ac1214@users.noreply.github.com>
2021-02-14 01:38:38 -0700

    temp commit

---
+++ src/components/CustomButton/Button.js
@@ -0,0 +1,53 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+
+// material-ui components
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+import styles from "assets/jss/material-dashboard-react/components/buttonStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function RegularButton(props) {
+  // console.log(props)
+  const classes = useStyles();
+  const {
+    color,
+    round,
+    children,
+    disabled,
+    simple,
+    size,
+    block,
+    link,
+    justIcon,
+    className,
+    muiClasses,
+    ...rest
+  } = props;
+  const btnClasses = classNames({
+    [classes.button]: true,
+    [classes[size]]: size,
+    [classes[color]]: color,
+    [classes.round]: round,
+    [classes.disabled]: disabled,
+    [classes.simple]: simple,
+    [classes.block]: block,
+    [classes.link]: link,
+    [classes.justIcon]: justIcon,
+    [className]: className
+  });
+
+  return (
+    <Button onClick={() => {
+      props.senddatatoparent(props.session_id);
+    }} {...rest} classes={muiClasses} className={btnClasses}>
+      {children}
+    </Button>
+  );
+}
+
